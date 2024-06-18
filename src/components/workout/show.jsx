d3528c5f16b59221f77229bb80d4008cb723c803
tommy-pellerin <tommytp@hotmail.fr>
2024-06-18 09:32:42 +0200

    on going

--- src/components/workout/show.jsx
+++ src/components/workout/show.jsx
@@ -1,7 +1,23 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { getData } from "../../services/data-fetch";
+import { useParams } from "react-router-dom";
 
 const WorkoutShow = () => {
   const [quantity,setQuantity] = useState(1)
+  const [workout, setWorkout] = useState([]);
+  const { workout_id } = useParams();
+  useEffect(() => {
+    const getWorkouts = async () => {
+      try {
+        const data = await getData(`/workouts/${workout_id}`);
+        console.log(data);
+        setWorkout(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getWorkouts();
+  }, [workout_id]);
 
   const increaseQuantity = () => {
     setQuantity(quantity + 1)
@@ -17,26 +33,30 @@ const WorkoutShow = () => {
 
       <div className="container mx-auto">
         <div className="grid grid-cols-3 gap-4 my-5">
-          
+
           <div className="col-span-2">
-            <div>
-              <h1>Titre</h1>
-              <p>petite description</p>
-              <div>Notes</div>
+            <div className="flex my-3">
+              <div>
+                <h1>{workout.title}</h1>
+                <p>petite description</p>
+              </div>
+              <div>Notes :</div>
             </div>
-            <div>
-              <p>Description</p>
-            </div>                      
+            <div className="my-3">
+              <p>Description:</p>
+              <p>{workout.description}</p>
+            </div>
           </div>
 
           <div className="col-span-1">
-            Reservation
-            <h2>Prix :</h2>
+            <h2>Prix : {workout.price}</h2>
             <p>Data</p>
-            <p>Debut</p>
-            <p>Fin</p>
-            <p>durée</p>
+            <p>Début de la séance :{workout.start_date}</p>
+            <p>Fin de la séance :</p>
+            <p>{workout.duration}</p>
             <div>
+              <p>Nombre de place max : {workout.max_participants}</p>
+              <p>Nombre de place disponible :</p>
               <p>Nombre de place : {quantity}</p>
               <div className="flex justify-around">
                 <button className="border" onClick={increaseQuantity}>+</button>
