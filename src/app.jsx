dad49a41b8bb8bea5417b6a2e66d6975fd0d12c3
tommy-pellerin <tommytp@hotmail.fr>
2024-06-19 23:20:57 +0200

    alert in App ok

--- src/app.jsx
+++ src/app.jsx
@@ -2,12 +2,25 @@ import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./components/static/routes";
 import Navbar from "./components/static/navbar";
 import Footer from "./components/static/footer";
+import Alert from "./styles/Alert";
+
+//Atom
+import { useAtom } from 'jotai';
+import { alertAtom } from "./store/alert";
 
 export default function App() {
+  const [alert, setAlert] = useAtom(alertAtom);
+  
   return (
     <BrowserRouter>
       <header>
         <Navbar />
+        <Alert 
+          showAlert={alert.showAlert} 
+          setShowAlert={setAlert} 
+          message={alert.message} 
+          type={alert.alertType} 
+        />
       </header>
       <main>
         <AppRoutes />
