6db3e6ae53c6d6906bbd40890b5d2b64f8a98dab
tommy-pellerin <tommytp@hotmail.fr>
2024-06-21 15:18:33 +0200

    token expired checked

--- src/services/tokenExpired.jsx
+++ src/services/tokenExpired.jsx
@@ -1,44 +1,20 @@
 import { useState, useEffect } from 'react';
-import { jwtDecode } from "jwt-decode";
 import { Navigate, useLocation } from 'react-router-dom';
 import Cookies from "js-cookie";
-
 import { useAtom } from "jotai";
 import { alertAtom } from "../store/alert";
+
+import checkTokenExpiration from './checkToken';
 
 const TokenExpirationCheck = ({ children }) => {
   const [isTokenExpired, setIsTokenExpired] = useState(false);
   const location = useLocation();
   const [,setAlert] = useAtom(alertAtom);
 
-  const checkTokenExpiration = () => {
-    console.log("token check");
-    let token = Cookies.get("keellauth"); // replace with your JWT token
-    if (token) {
-      token = token.replace("Bearer ", ""); // remove "Bearer " from the token
-      try {
-        console.log(token);
-        const decodedToken = jwtDecode(token);
-        console.log(decodedToken);
-        const dateNow = new Date();
-        const expirationDate = new Date(decodedToken.exp * 1000); // convert to milliseconds
-        console.log(`Token expiration date: ${expirationDate.toLocaleString()}`);
-
-        if (decodedToken.exp < dateNow.getTime() / 1000) {
-          setIsTokenExpired(true);
-        }
-      } catch (e) {
-        console.error("Invalid JWT token");
-        setIsTokenExpired(true);
-      }
-    } else {
-      console.error("No JWT token found");
+  useEffect(() => {
+    if(checkTokenExpiration()){
       setIsTokenExpired(true);
     }
-  };
-
-  useEffect(() => {
-    checkTokenExpiration();
   }, []);
 
   useEffect(() => {
