03bcedc3c8b985b1ce047a8ad326e858b658fbd1
tommy-pellerin <tommytp@hotmail.fr>
2024-06-25 17:53:32 +0200

    avatar on navbar

--- src/components/static/navbar.jsx
+++ src/components/static/navbar.jsx
@@ -127,7 +127,12 @@ export default function Navbar() {
                       {user.isLogged ?
                         (profile && 
                           <div className="h-8 w-8 border rounded-full flex justify-center items-center overflow-hidden">
-                            <img src={profile.avatar} style={{ width: '100%', height: '100%', objectFit: 'cover' }} alt="User avatar"/>
+                            {profile.avatar ? <img src={profile.avatar} style={{ width: '100%', height: '100%', objectFit: 'cover' }} alt="User avatar"/>
+                            :
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="size-6">
+                              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                            </svg>
+                            }
                           </div>
                         )
                         :
