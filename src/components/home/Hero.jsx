99ddc805897feca113dcd9e972fcfba7ca1c34ea
tommy-pellerin <tommytp@hotmail.fr>
2024-07-03 16:43:04 +0200

    home ok

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -3,15 +3,17 @@ import heroBackgroundImage from '../../assets/images/herobackgroundimage.png';
 
 const Hero = () => {
   return (
-    <div className="flex flex-col md:flex-row-reverse md:items-center">
-      <div className="md:w-1/2 flex justify-end"> 
-        <img src={heroBackgroundImage} alt="Hero Background" className="object-cover h-full w-full md:w-auto" />
-      </div>
-      <div className="md:w-1/3 flex flex-col justify-center items-center md:items-start text-center md:text-left p-4"> 
-        <h1 className="text-3xl md:text-4xl font-bold mb-4">Réservez, bougez, vivez !</h1>
-        <p className="max-w-md">
+    <div className="flex flex-col md:flex-row md:justify-between md:items-center bg-blue-500 md:h-[50vh] mb-5">
+      
+      <div className="w-full mx-auto md:w-1/3 flex flex-col items-center justify-center p-4">
+        <h1 className="text-white text-3xl md:text-7xl font-bold mb-4">Réservez, bougez, vivez !</h1>
+        <p className="max-w-md text-white">
           Découvrez et réservez la séance de sport parfaite pour votre prochaine aventure.
         </p>
+      </div>
+
+      <div className="md:w-1/2 flex justify-end bg-white h-full overflow-hidden"> 
+        <img src={heroBackgroundImage} alt="Hero Background" className="object-scale-down h-full w-full" />
       </div>
     </div>
   );
