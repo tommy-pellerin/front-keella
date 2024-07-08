7ce32df45b351ab7adb57b6080251e3a12763046
BrvAlexis <brevieralexis@gmail.com>
2024-07-08 16:50:20 +0200

    bg-blue-500 forget

--- src/components/static/pageNotFound.jsx
+++ src/components/static/pageNotFound.jsx
@@ -8,7 +8,7 @@ function PageNotFound() {
         <meta name="description" content="Page inexistante" />
       </Helmet>
       <div className="flex items-center justify-center h-screen bg-gray-200">
-        <div className="border border-purple-500 rounded-lg p-40 bg-white">
+        <div className="border border-blue-500 rounded-lg p-40 bg-white">
           <h1 className="text-8xl font-bold text-center">404</h1>
           <br />
           <h2 className="text-3xl">Page not found</h2>
