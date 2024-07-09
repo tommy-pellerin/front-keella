560df38f194a41cb3d680f2f6e6baca5c552a0f3
tommy-pellerin <tommytp@hotmail.fr>
2024-07-09 14:45:36 +0200

    shadow on navbar

--- src/components/static/navbar.jsx
+++ src/components/static/navbar.jsx
@@ -62,7 +62,7 @@ export default function Navbar() {
   }, [user]);
 
   return (
-    <Disclosure as="nav" className="">
+    <Disclosure as="nav" className="shadow-lg">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
