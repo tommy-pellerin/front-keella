2c022cc6b669ade4f5bafa8ca714d101e57862e1
BrvAlexis <brevieralexis@gmail.com>
2024-07-10 18:10:37 +0200

    avatar svg fix

--- src/components/rating/ProfilHostRatings.jsx
+++ src/components/rating/ProfilHostRatings.jsx
@@ -24,16 +24,15 @@ export default function RatingStars({ ratings, ratingsReceivedUserAvatars }) {
         return (
           <div key={rating.id} className="w-full max-w-md p-4 mb-4">
             <div className="flex items-center space-x-4">
-              {userAvatar ? (
-                <img src={userAvatar} alt="User avatar" className="w-8 h-8 rounded-full object-cover" />
-              ) : (
-                <div className="w-8 h-8 rounded-full bg-gray-200 flex justify-center items-center">
-                  {/* Icône SVG par défaut si aucun avatar n'est fourni */}
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-400">
+              <div className="h-8 w-8 border rounded-full flex justify-center items-center overflow-hidden">
+                {userAvatar ? (
+                  <img src={userAvatar} alt="User avatar" className="h-8 w-8 object-cover" />
+                ) : (
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="black" className="h-8 w-8">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                   </svg>
-                </div>
-              )}
+                )}
+              </div>
               <div className="flex flex-col">
                 <Link to={`/profile/${rating.user_id}`} className="text-blue-500 hover:underline">
                   {rating.user && rating.user.username ? rating.user.username : 'Unknown'}
